bb8577bc660fa36a1c68893658e4757783faeee1
lin6035-star <lin603526@gmail.com>
2026-07-07 19:06:35 +0800

    #fix

--- hello world/hello.java
+++ hello world/hello.java
@@ -1,9 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello World!!!  02User");
-=======
-        System.out.println("Hello World!!! JAVA");
->>>>>>> 312d4af6ad88332dfccc3da5b177cf218d0611ec
     }
 }
